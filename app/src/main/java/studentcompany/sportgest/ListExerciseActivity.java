0faaf9af77b9f24aad4e9cbb0113751be015acf6
andsil - Andre Silva <andresilvaptmail@gmail.com>
2015-11-29 03:43:06 +0000

    [Add] Display exercise activity [Add] Generic Pair adapter (incomplete and at the moment it has no use) [Add] Generic icons [Update] Player DAO corrected and UserDAO with other table name, since User is reserved keyword [Update] Exercise related activities (still with some bugs)

--- app/src/main/java/studentcompany/sportgest/ListExerciseActivity.java
+++ app/src/main/java/studentcompany/sportgest/ListExerciseActivity.java
@@ -19,11 +19,9 @@ import studentcompany.sportgest.daos.Attribute_DAO;
 import studentcompany.sportgest.daos.Attribute_Exercise_DAO;
 import studentcompany.sportgest.daos.Exercise_DAO;
 import studentcompany.sportgest.daos.exceptions.GenericDAOException;
-import studentcompany.sportgest.domains.EventCategory;
+import studentcompany.sportgest.domains.Exercise;
 
 public class ListExerciseActivity extends AppCompatActivity {
-    //Interface
-    private ListView listView;
 
     //DAOs
     private Exercise_DAO exercise_dao;
@@ -36,32 +34,32 @@ public class ListExerciseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list_exercise);
 
         exercise_dao = new Exercise_DAO(this);
-        ArrayList<EventCategory> eventCategorylist;
+        ArrayList<Exercise> exerciseList;
         try {
-            eventCategorylist = event_categorydao.getAll();
+            exerciseList = exercise_dao.getAll();
         } catch (GenericDAOException ex) {
             //System.err.println(ListEventCategoryActivity.class.getName() + " [WARNING] " + ex.toString());
             Logger.getLogger(ListEventCategoryActivity.class.getName()).log(Level.WARNING, null, ex);
-            eventCategorylist = null;
+            exerciseList = null;
         }
         ArrayList<String> array_list = new ArrayList<>();
-        for (EventCategory ec : eventCategorylist) {
-            array_list.add(ec.getName());
+        for (Exercise ec : exerciseList) {
+            array_list.add(ec.getTitle());
         }
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, array_list);
 
-        listView = (ListView) findViewById(R.id.event_category_ListView);
+        ListView listView = (ListView) findViewById(R.id.exercise_ListView);
         listView.setAdapter(arrayAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 // TODO Auto-generated method stub
-                int id_To_Search = arg2 + 1;
+                int id_To_Search = position + 1;
 
                 Bundle dataBundle = new Bundle();
-                dataBundle.putInt("id", id_To_Search);
+                dataBundle.putInt(Exercise_DAO.TABLE_NAME+Exercise_DAO.COLUMN_ID, id_To_Search);
 
-                Intent intent = new Intent(getApplicationContext(), DisplayEventCategoryActivity.class);
+                Intent intent = new Intent(getApplicationContext(), DisplayExerciseActivity.class);
 
                 intent.putExtras(dataBundle);
                 startActivity(intent);
@@ -69,7 +67,7 @@ public class ListExerciseActivity extends AppCompatActivity {
         });
 
         //Toolbar
-        Toolbar toolbar = (Toolbar) findViewById(R.id.list_event_category_toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.list_exercise_toolbar);
         setSupportActionBar(toolbar);
     }
 
@@ -77,27 +75,32 @@ public class ListExerciseActivity extends AppCompatActivity {
     public void onResume() {//update list
         super.onResume();  // Always call the superclass method first
 
-        ArrayList<EventCategory> eventCategorylist;
+        ArrayList<Exercise> exerciseList;
         try {
-            eventCategorylist = event_categorydao.getAll();
+            exerciseList = exercise_dao.getAll();
         } catch (GenericDAOException ex) {
             //System.err.println(ListEventCategoryActivity.class.getName() + " [WARNING] " + ex.toString());
             Logger.getLogger(ListEventCategoryActivity.class.getName()).log(Level.WARNING, null, ex);
-            eventCategorylist = null;
+            exerciseList = null;
         }
         ArrayList<String> array_list = new ArrayList<>();
-        for (EventCategory ec : eventCategorylist) {
-            array_list.add(ec.getName());
+        for (Exercise ec : exerciseList) {
+            array_list.add(ec.getTitle());
         }
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, array_list);
-        listView = (ListView) findViewById(R.id.event_category_ListView);
+
+        ListView listView = (ListView) findViewById(R.id.exercise_ListView);
         listView.setAdapter(arrayAdapter);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_list_event_category, menu);
+        getMenuInflater().inflate(R.menu.menu_crud, menu);
+        MenuItem editItem = menu.findItem(R.id.Edit);
+        editItem.setVisible(false);
+        MenuItem remItem = menu.findItem(R.id.Delete);
+        remItem.setVisible(false);
         return true;
     }
 
@@ -106,11 +109,11 @@ public class ListExerciseActivity extends AppCompatActivity {
         super.onOptionsItemSelected(item);
 
         switch (item.getItemId()) {
-            case R.id.Add_Event_Category:
+            case R.id.Add:
                 Bundle dataBundle = new Bundle();
-                dataBundle.putInt("id", 0);
+                dataBundle.putInt(Exercise_DAO.TABLE_NAME+Exercise_DAO.COLUMN_ID, 0);
 
-                Intent intent = new Intent(getApplicationContext(), DisplayEventCategoryActivity.class);
+                Intent intent = new Intent(getApplicationContext(), CreateExerciseActivity.class);
                 intent.putExtras(dataBundle);
 
                 startActivity(intent);
